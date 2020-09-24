f50acddd628e126186775c9eb4aa706615826da1
Станислав <stasychbr@mail.ru>
2020-09-24 18:42:25 +0300

    without extra budder

--- Threshold.cpp
+++ Threshold.cpp
@@ -16,29 +16,29 @@ std::vector<unsigned char> Threshold::pixMatrix(image_data& image, int row, int 
 }
 
 bool Threshold::run(image_data& image) {
-    unsigned char* bwImPix = new unsigned char[image.w * image.h * image.compPerPixel];
-    if (!bwImPix) {
+    //unsigned char* bwImPix = new unsigned char[image.w * image.h * image.compPerPixel];
+    /*if (!bwImPix) {
         return false;
-    }
-    memcpy(bwImPix, image.pixels, image.w * image.h * image.compPerPixel);
-    image_data bwIm;
+    }*/
+    //memcpy(bwImPix, image.pixels, image.w * image.h * image.compPerPixel);
+    /*image_data bwIm;
     bwIm.compPerPixel = image.compPerPixel;
     bwIm.h = image.h;
     bwIm.w = image.w;
-    bwIm.pixels = bwImPix;
-    if (!BlackWhite::run(bwIm)) {
+    bwIm.pixels = bwImPix;*/
+    if (!BlackWhite::run(image)) {
         return false;
     }
     for (int i = up(image); i < bottom(image); i++) {
         for (int j = left(image); j < right(image); j++) {
-            std::vector<unsigned char> intensMatrix = pixMatrix(bwIm, i, j);
-            unsigned char curIntense = bwImPix[(i * image.w + j) * image.compPerPixel];
+            std::vector<unsigned char> intensMatrix = pixMatrix(image, i, j);
+            unsigned char curIntense = image.pixels[(i * image.w + j) * image.compPerPixel];
             unsigned char median;
             int size = (int)intensMatrix.size();
             std::nth_element(intensMatrix.begin(), intensMatrix.begin() + size / 2, intensMatrix.end());
             if (intensMatrix.size() % 2 == 0) {
                 std::nth_element(intensMatrix.begin(), intensMatrix.begin() + size / 2 + 1, intensMatrix.end());
-                median = (intensMatrix[size / 2] + intensMatrix[size / 2 + 1]) / 2;
+                median = (intensMatrix[size / 2] + intensMatrix[size / 2 - 1]) / 2;
             }
             else {
                 median = intensMatrix[size / 2];
@@ -50,6 +50,6 @@ bool Threshold::run(image_data& image) {
             }
         }
     }
-    delete[] bwImPix;
+    //delete[] bwImPix;
     return true;
 }
