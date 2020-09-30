89215cd995de9d43369f03fce7c2dba16338b27b
Станислав <stasychbr@mail.ru>
2020-09-30 21:22:12 +0300

    try 229

--- Threshold.cpp
+++ Threshold.cpp
@@ -8,7 +8,7 @@ std::vector<unsigned char> Threshold::pixMatrix(image_data& image, int row, int 
     for (int i = row - 2; i <= row + 2; i++) {
         for (int j = col - 2; j <= col + 2; j++) {
             if (i >= up(image) && i < bottom(image) && j >= left(image) && j < right(image)) {
-                matrix.push_back(intensity(image, i, j)/*image.pixels[(i * image.w + j) * image.compPerPixel]*/);
+                matrix.push_back(image.pixels[(i * image.w + j) * image.compPerPixel]);
             }
         }
     }
@@ -26,14 +26,14 @@ bool Threshold::run(image_data& image) {
     bwIm.h = image.h;
     bwIm.w = image.w;
     bwIm.pixels = bwImPix;*/
-    /*if (!BlackWhite::run(image)) {
+    if (!BlackWhite::run(image)) {
         return false;
-    }*/
+    }
     std::vector<int> toErase;
     for (int i = up(image); i < bottom(image); i++) {
         for (int j = left(image); j < right(image); j++) {
             std::vector<unsigned char> intensMatrix = pixMatrix(image, i, j);
-            unsigned char curIntense = intensity(image, i, j); //image.pixels[(i * image.w + j) * image.compPerPixel];
+            unsigned char curIntense = image.pixels[(i * image.w + j) * image.compPerPixel]; //intensity(image, i, j); 
             unsigned char median;
             int size = (int)intensMatrix.size();
             std::nth_element(intensMatrix.begin(), intensMatrix.begin() + size / 2, intensMatrix.end());
